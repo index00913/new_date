df58fead81d63583b739068cdd6aaa7f00618a07
index00913 <jisu00913@gmail.com>
2023-09-21 10:26:25 +0900

    다음과 같이 코드를 줄일수 있음

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
-/*
-  객체를 생성하는 방법
-  - 객체리터럴을 통한 생성
-  - 생성자함수를 통해서 생성 (new 키워드를 이용해서 동일한 구조의 객체를 복사할때 주로 사용)
-  -생성자함수를 통해서 생성된 객체 : 인스턴스
-  -인스턴스 객체에는 prototype이라는 공통의 저장공간이 포함되어 있어서 자주쓰는  함수를 등록후 자유롭게 호출가능
-*/
+const [hr, m, s] = document.querySelectorAll('h1 span');
+
 const now = new Date();
 const hours = now.getHours();
 const min = now.getMinutes();
@@ -12,35 +7,25 @@ const sec = now.getSeconds();
 
 console.log(setNumbers(hours));
 
-function setNumbers(num) {
-	let result = 0;
-	/*
-	if (num < 10) result = '0' + num;
-	else result = num;
-	*/
-	//조건식 ? true일때 실행할 구문 : fals일때 실행할 구문
-	//삼항연산자 구문안에는 대임연산자가 들어올수 없기 때문에 괄호로 묶어서 표현식으로 변경
-	//특정값이 else 일때 굳이 실행할 코드가 없다면 &&연산자 활용
-	//num < 10 ? (result = '0' + num) : (result = num);
-	num < 10 && (num = '0' + num);
+//hours값을 setHours가 조₩건에 12를 뺀값으로 반환
+//반환된 결과값을 다시 바로 setNumbers에 집어넣어서 0을 붙이도록 처리
+hr.innerText = setNumbers(setHours(hours));
+m.innerText = setNumbers(min);
+s.innerText = setNumbers(sec);
 
+/* 다음과 같이 코드를 줄일수 있음
+const value = [setHours(hours), min, sec];
+spans.forEach((el, idx) => {
+	el.innerText = setNumbers();
+});
+*/
+
+function setNumbers(num) {
+	num < 10 && (num = '0' + num);
 	return num;
 }
 
-/*
-if (hours > 12) {
-	newHr = hours - 12;
-	if (newHr < 10) {
-		newHr = '0' + newHr + 'pm';
-	} else {
-		newHr = newHr + 'pm';
-	}
-} else {
-	newHr = hours;
-	if (newHr > 10) {
-		newHr = '0' + newHr + 'am';
-	} else {
-		newHr = newHr + 'am';
-	}
+function setHours(num) {
+	num > 12 && (num = num - 12);
+	return num;
 }
-*/
