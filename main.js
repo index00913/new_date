1e7ee39dd113ecab86f0fae3d137237a35b01ce7
index00913 <jisu00913@gmail.com>
2023-09-21 11:40:02 +0900

    사진 폰트 추가

--- main.js
+++ main.js
@@ -3,8 +3,18 @@
 	setInterval(반복실행할 함수, 반복간격시간(ms))
 */
 const [hr, m, s] = document.querySelectorAll('h1 span');
+setInterval(setTime, 1000);
 
-setInterval(() => {}, 1000);
+//함수 추가
+//원하는 시간마다 body에 morning, afternoon, evening, night
+//if(hour>=6 && hours< 11)document.body.classList.add('morging')
+
+function changeClass(hr) {
+	if (hr >= 6 && hr < 12) document.body.classList.add('morning');
+	if (hr >= 12 && hr < 16) document.body.classList.add('afternoon');
+	if (hr >= 16 && hr < 20) document.body.classList.add('evening');
+	if (hr >= 20 && hr < 6) document.body.classList.add('night');
+}
 
 function setTime() {
 	const now = new Date();
@@ -14,6 +24,7 @@ function setTime() {
 	hr.innerText = setNumbers(setHours(hours));
 	m.innerText = setNumbers(min);
 	s.innerText = setNumbers(sec);
+	changeClass(hours);
 }
 
 function setNumbers(num) {
