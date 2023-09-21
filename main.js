8be2072953ca6dd47691cb6711d1fb6aa3d1e45b
index00913 <jisu00913@gmail.com>
2023-09-21 09:29:08 +0900

    삼항연산자 구문안에는 대임연산자가 들어올수 없기 때문에 괄호로 묶어서 표현식으로 변경

--- main.js
+++ main.js
@@ -6,41 +6,39 @@
   -인스턴스 객체에는 prototype이라는 공통의 저장공간이 포함되어 있어서 자주쓰는  함수를 등록후 자유롭게 호출가능
 */
 const now = new Date();
-console.log(now);
-//우리나라 시간대는 전세계 표준시간대에서 9시간이 더해진 시간 (그리니치 표준시간대 적용)
-
-const months = [
-	'January',
-	'February',
-	'March',
-	'April',
-	'May',
-	'June',
-	'July',
-	'August',
-	'September',
-	'October',
-	'November',
-	'December',
-];
-
-const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'saturday'];
-
-const year = now.getFullYear();
-const month = now.getMonth();
-const date = now.getDate();
-const day = now.getDay();
 const hours = now.getHours();
 const min = now.getMinutes();
 const sec = now.getSeconds();
 
-console.log(hours);
+console.log(setNumbers(hours));
 
-let newHr = 0;
+function setNumbers(num) {
+	let result = 0;
+	/*
+	if (num < 10) result = '0' + num;
+	else result = num;
+	*/
+	//조건식 ? true일때 실행할 구문 : fals일때 실행할 구문
+	//삼항연산자 구문안에는 대임연산자가 들어올수 없기 때문에 괄호로 묶어서 표현식으로 변경
+	num < 10 ? (result = '0' + num) : (result = num);
 
-if (hours > 12) {
-	newHr = hours - 12 + 'pm';
-} else {
-	newHr = hours + 'am';
+	return result;
 }
-console.log(newHr);
+
+/*
+if (hours > 12) {
+	newHr = hours - 12;
+	if (newHr < 10) {
+		newHr = '0' + newHr + 'pm';
+	} else {
+		newHr = newHr + 'pm';
+	}
+} else {
+	newHr = hours;
+	if (newHr > 10) {
+		newHr = '0' + newHr + 'am';
+	} else {
+		newHr = newHr + 'am';
+	}
+}
+*/
